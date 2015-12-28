4a5b3e6a6a86c2257867dbd97e86b784d644a849
Joe <Joe@Joe-PC>
2015-12-28 11:56:59 +0000

    Change color in CSS to be darker. Updated page tab names.

--- list_delete.php
+++ list_delete.php
@@ -4,7 +4,7 @@ $starttime=microtime(true);
 <!doctype html>
 <html>
 <head>
-  <title>TS-N.NET Channeldeleter - List next deletes</title>
+  <title>Teamspeak Channel Deletion List</title>
   <meta http-equiv="content-type" content="text/html; charset=utf-8">
   <link rel="stylesheet" href="style.css" type="text/css">
 </head>  
